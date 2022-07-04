fc48b5748c174d2e056b114974f43897456c764c
gorkemdurgun <gorkemdurgunn@gmail.com>
2022-07-04 17:24:17 +0300

    x not showing

---
+++ src/components/Board.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Square from "./Square";
+
+class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(e){
+        const table = this.state.squares.slice();
+        table[e] = 'X';
+        this.setState({squares: table});
+    }
+
+    renderSquare(e) {
+        return (
+            <Square
+                value={this.state.squares[e]}
+                onClick={()=> this.handleClick(e)}
+            />
+        );
+    }
+
+    render() {
+        const status = 'Next Player: X'
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Board;
---
+++ src/components/Square.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+        return (
+
+            <button
+                className="square"
+                onClick={async () => {
+                    await this.props.onClick;
+                    console.log(this.props.value)
+                }}>
+                {this.state.value}
+            </button>
+
+        );
+    }
+
+}
+
+export default Square;
